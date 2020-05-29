963397999435f67a172ca8d80674a8746a17cc10
Victor Reinor <victorreinor@gmail.com>
2020-05-28 22:34:26 -0300

    habilitando roteamento com html5

--- app/js/app.js
+++ app/js/app.js
@@ -1,7 +1,7 @@
 'use strict';
 
 var eventsApp = angular.module('eventsApp', ['ngResource', 'ngRoute'])
-  .config(function ($routeProvider) {
+  .config(function ($routeProvider, $locationProvider) {
     $routeProvider.when('/newEvent',
       {
         templateUrl: 'templates/NewEvent.html',
@@ -21,6 +21,7 @@ var eventsApp = angular.module('eventsApp', ['ngResource', 'ngRoute'])
       });
 
     $routeProvider.otherwise({ redirectTo: '/events' });
+    $locationProvider.html5Mode(true);
   })
   .factory('myCache', function ($cacheFactory) {
     return $cacheFactory('myCache', { capacity: 3 });
