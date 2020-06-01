c5b3dbfc482c47432e47408faa7ced8d77822c8f
Victor Reinor <victorreinor@gmail.com>
2020-06-01 19:58:22 -0300

    adiciona exemplo utilizado location url

---
+++ app/js/controllers/MainMenuController.js
@@ -0,0 +1,9 @@
+'use strict';
+
+eventsApp.controller('MainMenuController', 
+  function MainMenuController($scope, $location) {
+    $scope.createEvent = function() {
+      $location.url('/newEvent');
+    }
+  }
+);
